1b8ea20b3fdb2d7ad2e772ffe9167418e02c8cab
trancongduu <126306009+trancongduu@users.noreply.github.com>
2024-06-19 19:36:39 +0700

    Update owl-carousel.js

--- owl-carousel.js
+++ owl-carousel.js
@@ -1,1 +1,44 @@
-$(document).ready(function() {   $('.projects_collection_list').owlCarousel({     items: 3,     margin: 20,     loop: true,     autoplay: true,     autoplayTimeout: 3000,     autoplayHoverPause: true,     nav: true,     dots: true,     responsive: {       0: {         items: 1,       },       600: {         items: 2,       },       1000: {         items: 3,       }     }   }); });
+function slider2() {
+
+    let splides = $('.slider2');
+    for ( let i = 0, splideLength = splides.length; i < splideLength; i++ ) {
+        new Splide( splides[ i ], {
+
+        perPage: 5,
+        perMove: 1,
+      focus: 'center',
+      type: 'loop', 
+      gap: '4em', 
+      arrows: 'slider', 
+      pagination: 'slider', 
+      speed : 550, 
+      dragAngleThreshold: 80, 
+      autoWidth: false, 
+      rewind : false, 
+      rewindSpeed : 800,
+      waitForTransition : false,
+      updateOnMove : true,
+      trimSpace: false,
+      breakpoints: {
+            991: {
+            
+                perPage: 3,
+          gap: '4vw',
+            },
+        767: {
+            
+                perPage: 1,
+          gap: '4vw',
+            },
+        479: {
+            
+                perPage: 1,
+          gap: '4vw',
+            }
+        }
+    } ).mount();
+    }
+    
+    }
+    slider2();
+    
